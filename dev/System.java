38d6a0f60afb9c3e65c2ff747e3d3f8bd44a2807
ChenFryd <chenfrydman@gmail.com>
2023-03-21 18:37:54 +0200

    added menus

--- dev/System.java
+++ dev/System.java
@@ -1,6 +1,24 @@
 public class System {
-   
-    public void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        int choice = 1;
+        while (choice != 0) {
+            printMenu();
+            ch
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Hello to the HR system!");
+        System.out.println("Please select the following options");
+        System.out.println("1. add new employee");
+        System.out.println("2. add new store");
+    }
+
+    public static void printEmployeeMenu() {
+        System.out.println("What kind of Employee do you want to add?");
+        System.out.println("1. HR manager");
+        System.out.println("2. Casier");
+        System.out.println("3. Warehouse Employee");
     }
 }
