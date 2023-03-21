52d9d0b94a79f89c50c7b18a706260ad3fd5a80c
ChenFryd <chenfrydman@gmail.com>
2023-03-21 18:24:04 +0200

    created necessary files

--- dev/Employee.java
+++ dev/Employee.java
@@ -1,3 +1,21 @@
+import java.util.Map;
+
 public abstract class Employee{
-    
+    private String first_name;
+    private String last_name;
+    private int age;
+    private int id;
+    private int bank_account;
+    private Map<String,int>[] time;
+    public Employee(String first_name, String last_name, int age, int id, int bank_account){
+        this.first_name=first_name;
+        this.last_name=last_name;
+        this.age=age;
+        this.id=id;
+        this.bank_account=bank_account;
+    }
+    public void canWork(Map<String,int>[] arr_can_work){
+        this.time = arr_can_work;
+    }
+
 }
---
+++ dev/HRManager.java
@@ -0,0 +1,8 @@
+public class HRManager extends Employee{
+    public HRManager(String first_name, String last_name, int age, int id, int bank_account){
+        super(first_name, last_name, age, id, bank_account));
+
+    }
+}
+    
+
