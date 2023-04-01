73d7b59d8f996261f00c2617af72e1a6c36e2fe7
Danielshapira12 <dashap@post.bgu.ac.il>
2023-04-02 02:16:55 +0300

    tests directory

---
+++ Test/EmployeeTest.java
@@ -0,0 +1,50 @@
+import Roles.CleanerRole;
+import Roles.IRole;
+import Roles.SecurityRole;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void addStore() {
+        Employee employee = new Employee("daniel", "shapira", 26, 209876676, "234657");
+        Store store = new Store("a", "b");
+        employee.addStore(store);
+        assertEquals(store, employee.getStores().get(0));
+    }
+
+    @Test
+    void get_first_name() {
+        Employee employee = new Employee("daniel", "shapira", 26, 209876676, "234657");
+        assertEquals("daniel", employee.get_first_name());
+    }
+
+    @Test
+    void setRole() {
+        CleanerRole cleanerRole = new CleanerRole();
+        Employee employee = new Employee("daniel", "shapira", 26, 209876676, "234657");
+        assertTrue(employee.setRole(cleanerRole));
+
+    }
+
+    @Test
+    void getRoles() {
+        CleanerRole cleanerRole = new CleanerRole();
+        SecurityRole securityRole = new SecurityRole();
+        Employee employee = new Employee("daniel", "shapira", 26, 209876676, "234657");
+        employee.setRole(cleanerRole);
+        employee.setRole(securityRole);
+        assertEquals(cleanerRole, employee.getRoles().get(0));
+        assertEquals(securityRole,employee.getRoles().get(1));
+    }
+
+    @Test
+    void getStores() {
+    }
+
+    @Test
+    void removeStore() {
+    }
+}
