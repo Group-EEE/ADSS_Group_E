1d991fdad7217c63e2d7495feea33c378da55f51
SapirTzaig <tzaigsap@post.bgu.ac.il>
2023-04-03 17:34:32 +0300

    3.4.23 tests

---
+++ dev/tests/CategoryControllerTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @Test
+    void addCategory() {
+    }
+
+    @Test
+    void addSubCategory() {
+    }
+
+    @Test
+    void addSubSubCategory() {
+    }
+
+    @Test
+    void removeCategory() {
+    }
+
+    @Test
+    void removeSubCategory() {
+    }
+
+    @Test
+    void removeSubSubCategory() {
+    }
+
+    @Test
+    void check_if_exist_cat() {
+    }
+
+    @Test
+    void check_if_exist_subcat() {
+    }
+
+    @Test
+    void check_if_exist_subsubcat() {
+    }
+
+    @Test
+    void printCategorysInSystem() {
+    }
+}
