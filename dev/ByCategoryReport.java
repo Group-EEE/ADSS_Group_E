7e8cd46394435121e3776233c4daacc36b7cb3ae
LironShemen <lironmir@post.bgu.ac.il>
2023-04-06 13:12:49 +0300

    documentation 6.4

--- dev/ByCategoryReport.java
+++ dev/ByCategoryReport.java
@@ -3,14 +3,18 @@ import java.util.List;
 public class ByCategoryReport extends Report{
 
     //constructor -  creates a report by category according to the user chosen categories
+    //the constructor will print the report
     public ByCategoryReport(String reporter, List<String> Categories){
-        super(reporter);
+        super(reporter); //inherit from abstract report class
         System.out.println("***** By Category Products Report *****");
         System.out.println("Reporter : "+ reporter);
-        for(int j=0; j<Categories.size();j++){
-            System.out.println("Category Name: " + Categories.get(j));
-            for(int i=0; i<ProductController.getProducts().size();i++){
+        for(int j=0; j<Categories.size();j++){ //for every category in the store, so the report will be
+            //by category
+            System.out.println("Category Name: " + Categories.get(j)); //print category name
+            for(int i=0; i<ProductController.getProducts().size();i++){ //for every product in the store
+                //if the product belongs to the current category
                 if(ProductController.getProducts().get(i).getCategory().compareTo(Categories.get(j))==0){
+                  //print the details of this product
                     System.out.println("Product's Barcode: " +ProductController.getProducts().get(i).getBarcode() +
                             " Product's Name: " + ProductController.getProducts().get(i).getPName() +
                             " Product's Amount: " + ProductController.getProducts().get(i).getSpecificProducts().size());
@@ -18,9 +22,9 @@ public class ByCategoryReport extends Report{
             }
 
         }
-        ReportController.addReport(this);
+        ReportController.addReport(this); //add the report to report controller to save its details
     }
-//this function returns the report type
+//this function returns the report type, used to print all reports' information
     @Override
     public String getReportType() {
         return "Product By Category Report";
--- dev/CategoryController.java
+++ dev/CategoryController.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 // this class controls all categories, subcategories and subsubcategories that exist in "SuperLi"
+//in this class we save all the information about them
 public class CategoryController {
     private static List<Category> categories; // list of all categories in system
     private static List<SubCategory> subcategories; // list of all subcategories in system
@@ -36,10 +37,10 @@ public class CategoryController {
 
     //this function adds subcategory to the subcategories list
     public static void addSubCategory(String catname, String subcatname){
-        SubCategory sc = new SubCategory(subcatname);
-        for(int i=0; i<categories.size(); i++){
+        SubCategory sc = new SubCategory(subcatname); //create a subcategory
+        for(int i=0; i<categories.size(); i++){ //looking for its main category
             if(categories.get(i).getName().compareTo(catname)==0){
-                categories.get(i).addSub(sc);
+                categories.get(i).addSub(sc); //add the subcategory to the main category's list
             }
         }
         subcategories.add(sc);
@@ -48,58 +49,60 @@ public class CategoryController {
     //this function adds subsubcategory to the subsubcategories list
     public static void addSubSubCategory(String subcatname, String subsubcatname){
         SubSubCategory ssc = new SubSubCategory(subsubcatname);
-        for(int i=0; i<subcategories.size(); i++){
+        for(int i=0; i<subcategories.size(); i++){ //looking for its subcategory
             if(subcategories.get(i).getName().compareTo(subcatname)==0){
-                subcategories.get(i).addSubSub(ssc);
+                subcategories.get(i).addSubSub(ssc); //add the subcategory to the subcategory's list
             }
         }
         subSubCategories.add(ssc);
     }
 
     //this function removes category from the categories list
-
     public void removeCategory(String name){
-        boolean exist = false;
+        boolean exist = false; //if there is a product that belongs to this category- will be true
         Category c = null;
-        for(int i=0;i<categories.size();i++){
+        for(int i=0;i<categories.size();i++){ //find the category by its name in the list
             if(categories.get(i).getName().compareTo(name)==0){
-                 c = categories.get(i);
+                 c = categories.get(i); //the category found and saved, so we can remove it if need
                 for(int j=0; j< ProductController.getProducts().size(); j++){
+                    //check for every product in ths store if its category is the given category
                     if(ProductController.getProducts().get(j).getCategory().compareTo(name)==0){
                         exist = true;
                     }
                 }
             }
         }
-        if(exist==false){
+        if(exist==false){ //we couldnt find product that belongs to this category
             categories.remove(c);
         }
-        else{
+        else{ //exist = true!
             System.out.println("Can't remove category");
         }
     }
 
     //this function removes subcategory from the subcategories list
     public void removeSubCategory(String subname, String catname){
-        boolean exist = false;
+        boolean exist = false; //if there is a product that belongs to this subcategory- will be true
         SubCategory c = null;
-        for(int i=0;i<subcategories.size();i++){
+        for(int i=0;i<subcategories.size();i++){ //find the subcategory by its name in the list
             if(subcategories.get(i).getName().compareTo(subname)==0){
-                c = subcategories.get(i);
+                c = subcategories.get(i); //the subcategory found and saved, so we can remove it if need
                 for(int j=0; j< ProductController.getProducts().size(); j++){
+                    //check for every product in ths store if its subcategory is the given subcategory
                     if(ProductController.getProducts().get(j).getSubCategory().compareTo(subname)==0){
                         exist = true;
                     }
                 }
             }
         }
-        if(exist==false){
-            for(int  j=0;j<CategoryController.categories.size();j++){
-                if(CategoryController.categories.get(j).getName().compareTo(catname)==0){
+        if(exist==false){ //we couldnt find product that belongs to this sucategory
+            for(int  j=0;j<CategoryController.categories.size();j++){//find the main category of the subcategory
+                if(CategoryController.categories.get(j).getName().compareTo(catname)==0){ //the main category
+                    //remove the subcategory from the main category's list
                     CategoryController.categories.get(j).getSubCategories().remove(c);
                 }
             }
-            subcategories.remove(c);
+            subcategories.remove(c); //remove the subcategory from the controllers' list
         }
         else{
             System.out.println("Can't remove subcategory");
@@ -108,25 +111,28 @@ public class CategoryController {
 
     //this function removes subsubcategory from the subsubcategories list
     public void removeSubSubCategory(String subsubname,String subname, String catname){
-        boolean exist = false;
+        boolean exist = false;//if there is a product that belongs to this subsubcategory- will be true
         SubSubCategory c = null;
-        for(int i=0;i<subSubCategories.size();i++){
+        for(int i=0;i<subSubCategories.size();i++){//find the subsubcategory by its name in the list
             if(subSubCategories.get(i).getName().compareTo(subsubname)==0){
-                c = subSubCategories.get(i);
+                c = subSubCategories.get(i);//the subsubcategory found and saved, so we can remove it if need
                 for(int j=0; j< ProductController.getProducts().size(); j++){
+                    //check for every product in ths store if its subcategory is the given subsubcategory
                     if(ProductController.getProducts().get(j).getSubSubCategory().compareTo(subsubname)==0){
                         exist = true;
                     }
                 }
             }
         }
-        if(exist==false){
+        if(exist==false){//we couldnt find product that belongs to this subsucategory
+            //find the subcategory of the subsubcategory
             for(int  j=0;j<CategoryController.subcategories.size();j++){
                 if(CategoryController.subcategories.get(j).getName().compareTo(catname)==0){
+                    //remove the subsubcategory from the subcategory's list
                     CategoryController.subcategories.get(j).getSubSubCategories().remove(c);
                 }
             }
-            subSubCategories.remove(c);
+            subSubCategories.remove(c);//remove the subsubcategory from the controllers' list
         }
         else{
             System.out.println("Can't remove subsubcategory");
@@ -136,7 +142,7 @@ public class CategoryController {
     //this function checks if the category exist in the system
     public static boolean check_if_exist_cat(String cat_name){
        if(categories!=null){
-           for(int i=0; i<categories.size(); i++){
+           for(int i=0; i<categories.size(); i++){ //check every category in the controller's list
                if(categories.get(i).getName().compareTo(cat_name)==0){
                    return true;
                }
