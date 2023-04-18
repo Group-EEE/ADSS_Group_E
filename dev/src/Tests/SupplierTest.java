771dfb6cb0a81a3364c245e58511d98679376c0c
odedati <127615093+odedati@users.noreply.github.com>
2023-04-18 12:12:48 +0300

    upload src

---
+++ dev/src/Tests/SupplierTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierTest {
+
+    @Test
+    void is_supplier() {
+        Supplier supplier = new Supplier("Ben Gurion", "054876542", "Osem", "David Shafir");
+        assertFalse(supplier.is_logistical_center());
+        assertTrue(supplier.is_supplier());
+        assertFalse(supplier.is_store());
+    }
+
+    @Test
+    void getSupplier_n() {
+        Supplier supplier = new Supplier("Ben Gurion", "054876542", "Osem", "David Shafir");
+        assertEquals("Osem", supplier.getSite_name());
+    }
+
+    @Test
+    void setSupplier_n() {
+        Supplier supplier = new Supplier("Ben Gurion", "054876542", "Osem", "David Shafir");
+        supplier.setSupplier_name("Tnuva");
+        assertEquals("Tnuva", supplier.getSupplier_name());
+        assertNotEquals("Osem", supplier.getSupplier_name());
+    }
+}
