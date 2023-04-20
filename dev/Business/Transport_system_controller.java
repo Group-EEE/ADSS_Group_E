51df77ebc65ec3c7a236b1afcc870cad91fb5633
galevi7 <127654104+galevi7@users.noreply.github.com>
2023-04-20 13:30:10 +0300

    create Transport_system_controller

---
+++ dev/Business/Transport_system_controller.java
@@ -0,0 +1,8 @@
+package Business;
+
+public class Transport_system_controller {
+    private Transport_System transport_system;
+    public Transport_system_controller(){
+        transport_system = new Transport_System();
+    }
+}
