2703700695f08ed87651f94eae4d31a887203ca2
Yehonatan28 <kidushim@post.bgu.ac.il>
2023-04-21 16:13:25 +0300

    21.4.23

--- dev/src/Main.java
+++ dev/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args)
     {
         SupplierModulePresentation supplierModulePresentation = new SupplierModulePresentation();
+        //InventoryModulePresentation supplierModulePresentation = new InventoryModulePresentation();
+        String yourChoice = "";
+        while (!yourChoice.equals("0")) {
+            System.out.println("\nPlease choose one of the options shown in the menu:\n");
+            System.out.println("1. Suppliers.");
+            System.out.println("2. Inventory.");
+            System.out.println("0. Exit.");
+
+        }
         supplierModulePresentation.PowerOn();
     }
 
