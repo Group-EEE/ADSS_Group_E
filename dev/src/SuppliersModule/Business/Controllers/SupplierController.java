1be97909ed170957c6f8a2400f278cd91ae52fc2
AlonAzran <127650722+AlonAzran@users.noreply.github.com>
2023-04-29 17:10:21 +0300

    29.04 17:10 Alon

--- dev/src/SuppliersModule/Business/Controllers/SupplierController.java
+++ dev/src/SuppliersModule/Business/Controllers/SupplierController.java
@@ -16,7 +16,7 @@ public class SupplierController {
     private SupplierController() {
 
         superLeeDB = SuperLeeDB.getInstance();
-        //productController = ProductController.getInstance();
+        productController = ProductController.getInstance();
     }
 
     public static SupplierController getInstance() {
@@ -30,7 +30,7 @@ public class SupplierController {
     }
 
     public boolean checkIfSupplierSupplyProduct(String supplierNum, int barcode){
-        GenericProduct genericProduct = ProductsByBarcode.get(barcode);
+        GenericProduct genericProduct = superLeeDB.getGenericProductByBarcode(barcode);
         if(genericProduct == null)
             return false;
         return genericProduct.checkIfSupplierSupplyTheProduct(supplierNum);
@@ -58,7 +58,7 @@ public class SupplierController {
         Supplier supplier = getSupplier(supplierNum);
         GenericProduct genericProduct = superLeeDB.getGenericProductByName(productName, manufacturerName);
 
-        //ProductController.BarcodesOfNewProducts.add(genericProduct.getBarcode());
+        ProductController.BarcodesOfNewProducts.add(genericProduct.getBarcode());
 
         SupplierProduct supplierProduct = new SupplierProduct(price, supplierCatalog, amount,
                 supplier, genericProduct, supplier.getMyAgreement());
