c6973d5774cc5fa9c8a6a5abccc64614942c9dc8
ChenFryd <chenfrydman@gmail.com>
2023-05-01 10:11:13 +0300

    for some reason they got deleted

--- dev/ADSS/src/DataAccessLayer/HRMoudle/StoresDAO.java
+++ dev/ADSS/src/DataAccessLayer/HRMoudle/StoresDAO.java
@@ -1,9 +1,26 @@
 package DataAccessLayer.HRMoudle;
 
-public class StoresDAO {
+import BussinessLayer.HRModule.Objects.Store;
+import DataAccessLayer.DAO;
+
+import java.sql.*;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.List;
+
+public class StoresDAO extends DAO {
     private static StoresDAO _storesDAO = null;
+    private final HashMap<Integer,Store> storesCache;
+    private final HashMap<String,Store> storesCacheByName;
+
+    public static final String StoreIDColumnName = "storeID";
+    public static final String NameColumnName = "name";
+    public static final String AddressColumnName = "address";
 
     private StoresDAO() {
+        super("Stores");
+        storesCache = new HashMap<>();
+        storesCacheByName = new HashMap<>();
     }
 
     public static StoresDAO getInstance() {
@@ -11,6 +28,142 @@ public class StoresDAO {
             _storesDAO = new StoresDAO();
         return _storesDAO;
     }
+
+    @Override
+    public boolean Insert(Object storeObj) {
+        Store store = (Store) storeObj;
+        boolean res = true;
+        String sql = MessageFormat.format("INSERT INTO {0} ({1}, {2}, {3}) VALUES(?, ?, ?) "
+                , _tableName, StoreIDColumnName, NameColumnName, AddressColumnName
+        );
+
+        try (Connection connection = DriverManager.getConnection(url);
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            //Connection connection=open
+            //pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, store.getStoreID());
+            pstmt.setString(2, store.getName());
+            pstmt.setString(3, store.getAddress());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Got Exception:");
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            res = false;
+        }
+        storesCache.put(store.getStoreID(), store);
+        storesCacheByName.put(store.getName(), store);
+        return res;
+    }
+
+    @Override
+    public boolean Delete(Object storeObj) {
+        Store store = (Store) storeObj;
+        boolean res = true;
+        String sql = MessageFormat.format("DELETE FROM {0} WHERE {1} = ? "
+                , _tableName, StoreIDColumnName);
+
+        try (Connection connection = DriverManager.getConnection(url);
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, store.getStoreID());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Got Exception:");
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            res = false;
+        }
+        storesCache.remove(store.getStoreID());
+        storesCacheByName.remove(store.getName());
+        return res;
+    }
+
+    @Override
+    public Store convertReaderToObject(ResultSet rs) throws SQLException {
+        if(storesCache.containsKey(rs.getInt(1)))
+            return storesCache.get(rs.getInt(1));
+        Store store = new Store(rs.getInt(1), rs.getString(2), rs.getString(3));
+        storesCache.put(store.getStoreID(),store);
+        storesCacheByName.put(store.getName(),store);
+        return store;
+    }
+
+    public int getStoreIDByName(String storeName){
+        if (storesCacheByName.containsKey(storeName))
+            return storesCacheByName.get(storeName).getStoreID();
+        List<ResultSet> rsList = Select(StoreIDColumnName);
+        for (ResultSet rs : rsList) {
+            try {
+                if (rs.getString(2).equals(storeName)) {
+                    return rs.getInt(1);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
+
+    public boolean existsStore(int storeID){
+        if (storesCache.containsKey(storeID))
+            return true;
+        List<ResultSet> rsList = Select(StoreIDColumnName);
+        for (ResultSet rs : rsList) {
+            try {
+                if (rs.getInt(1) == storeID) {
+                    return true;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean existsStore(String storeName){
+        if (storesCacheByName.containsKey(storeName))
+            return true;
+        List<ResultSet> rsList = Select(StoreIDColumnName);
+        for (ResultSet rs : rsList) {
+            try {
+                if (rs.getString(2).equals(storeName)) {
+                    return true;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public Store getStore(int storeID) {
+        if (storesCache.containsKey(storeID))
+            return storesCache.get(storeID);
+        else {//Employee in db
+            List<Store> result = Select(makeList(StoreIDColumnName), makeList(String.valueOf(storeID)));
+            if (result.size() == 0)
+                throw new IllegalArgumentException("store " +storeID + " does not exist");
+            Store store = result.get(0);
+            storesCache.put(storeID,store);
+            storesCacheByName.put(store.getName(),store);
+            return store;
+        }
+    }
+
+    public Store getStore(String storeName) {
+        if (storesCacheByName.containsKey(storeName))
+            return storesCacheByName.get(storeName);
+        else {//Employee in db
+            List<Store> result = Select(makeList(NameColumnName), makeList(storeName));
+            if (result.size() == 0)
+                throw new IllegalArgumentException("store " +storeName + " does not exist");
+            Store store = result.get(0);
+            storesCache.put(store.getStoreID(),store);
+            storesCacheByName.put(storeName,store);
+            return store;
+        }
+    }
+
 
 
 }
