86e4fe5b7549257f3df445c01bf8ac4041d67c35
ChenFryd <chenfrydman@gmail.com>
2023-05-01 18:32:25 +0300

    fixed creating first time system

--- dev/ADSS/src/BussinessLayer/HRModule/Controllers/EmployeeController.java
+++ dev/ADSS/src/BussinessLayer/HRModule/Controllers/EmployeeController.java
@@ -110,16 +110,13 @@ public class EmployeeController {
      * @param employeeID - the id of the employee
      * for HRMenuRemoveRoleFromEmployee
      */
-    public boolean printEmployeeRoles(int employeeID) {
+    public String printEmployeeRoles(int employeeID) {
         if (employeeID <0)
             throw new IllegalArgumentException("Illegal employee ID");
         Employee employee = getEmployeeByID(employeeID);
         if (employee == null)
             throw new IllegalArgumentException("Employee not found");
-        for (int i=1; i<=employee.getRoles().size(); i++) {
-            System.out.println(i + ". " + employee.getRoles().get(i-1));
-        }
-        return true;
+        return new Gson().toJson(employee.getRoles());
     }
 
     /**
@@ -129,13 +126,10 @@ public class EmployeeController {
     public boolean removeEmployee(int employeeID) {
         if (employeeID <0)
             throw new IllegalArgumentException("Illegal employee ID");
-        Employee employee = getEmployeeByID(employeeID);
-        if (employee == null) {
-            throw new IllegalArgumentException("Employee not found");
-        }
         _employeesDAO.Delete(employeeID);
         _passwordsDAO.Delete(employeeID);
-        //_employeesToStoreDAO.Delete(employee); //TODO
+        _employeesToStoreDAO.Delete(employeeID,false);
+        _employeesToRolesDAO.Delete(employeeID);
 
         return true;
     }
@@ -167,6 +161,7 @@ public class EmployeeController {
         Employee employee = getEmployeeByID(employeeID);
         if (employee == null)
             throw new IllegalArgumentException("employee not found");
+        _employeesToRolesDAO.Insert(new Pair<Integer,String>(employeeID, role.toString()));
         return employee.addRole(role);
     }
 
@@ -183,7 +178,7 @@ public class EmployeeController {
         Employee employee = getEmployeeByID(employeeID);
         if (employee == null)
             throw new IllegalArgumentException("employee not found");
-        //_employeesToStoreDAO.(employee).add(store); //TODO: fix
+        _employeesToStoreDAO.Insert(new Pair<Integer,Integer>(employeeID, store.getStoreID()));
         return true;
     }
 
@@ -195,8 +190,7 @@ public class EmployeeController {
         Employee employee = getEmployeeByID(employeeID);
         if (employee == null)
             throw new IllegalArgumentException("employee not found");
-        //_employeeStoreMap.get(employee).remove(store); //TODO
-        return true;
+        return _employeesToStoreDAO.Delete(new Pair<Integer,Integer>(employeeID, store.getStoreID()));
     }
 
     public String printEmployees() {
