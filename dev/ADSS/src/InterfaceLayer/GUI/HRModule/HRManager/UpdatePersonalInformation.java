ae8b0ccf2b6b28f8441d2adcf6e68601a3590431
Danielshapira12 <dashap@post.bgu.ac.il>
2023-06-13 00:52:00 +0300

    continued gui

---
+++ dev/ADSS/src/InterfaceLayer/GUI/HRModule/HRManager/UpdatePersonalInformation.java
@@ -0,0 +1,128 @@
+package InterfaceLayer.GUI.HRModule.HRManager;
+
+import BussinessLayer.HRModule.Controllers.EmployeeController;
+//TODO: FIX EXCEPTIONS
+
+import BussinessLayer.HRModule.Controllers.Facade;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UpdatePersonalInformation extends JFrame {
+    private final EmployeeController employeeController = EmployeeController.getInstance();
+
+    private final int hrManagerID;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField bankAccountField;
+
+    public UpdatePersonalInformation() {
+
+        this.hrManagerID = employeeController.getHRManagerID();
+
+        // Set the size and layout of the frame
+        setSize(400, 200);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        // Create labels and text fields
+        JLabel idLabel = new JLabel("HR Manager ID:");
+        JLabel firstNameLabel = new JLabel("First Name:");
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        JLabel bankAccountLabel = new JLabel("Bank Account:");
+
+        firstNameField = new JTextField(20);
+        lastNameField = new JTextField(20);
+        bankAccountField = new JTextField(20);
+
+        // Add labels and text fields to the frame
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        getContentPane().add(idLabel, gbc);
+
+        gbc.gridx = 1;
+        getContentPane().add(new JLabel(String.valueOf(hrManagerID)), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        getContentPane().add(firstNameLabel, gbc);
+
+        gbc.gridx = 1;
+        getContentPane().add(firstNameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        getContentPane().add(lastNameLabel, gbc);
+
+        gbc.gridx = 1;
+        getContentPane().add(lastNameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        getContentPane().add(bankAccountLabel, gbc);
+
+        gbc.gridx = 1;
+        getContentPane().add(bankAccountField, gbc);
+
+        // Create an "Update" button and add an ActionListener
+        JButton updateButton = new JButton("Update");
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 0, 0, 0);
+        getContentPane().add(updateButton, gbc);
+
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int confirmation = JOptionPane.showConfirmDialog(UpdatePersonalInformation.this, "Are you sure you want to update your personal information?", "Confirmation", JOptionPane.YES_NO_OPTION);
+                if (confirmation == JOptionPane.YES_OPTION) {
+                    String firstName = firstNameField.getText();
+                    String lastName = lastNameField.getText();
+                    String bankAccount = bankAccountField.getText();
+
+                    // Check which fields are updated and call the appropriate update methods
+                    boolean success = true;
+                    if (!firstName.isEmpty()) {
+                        if (!employeeController.updateFirstName(hrManagerID, firstName)) {
+                            success = false;
+                        }
+                    }
+                    if (!lastName.isEmpty()) {
+                        if (!employeeController.updateLastName(hrManagerID, lastName)) {
+                            success = false;
+                        }
+                    }
+                    if (!bankAccount.isEmpty()) {
+                        if (!employeeController.updateBankAccount(hrManagerID, bankAccount)) {
+                            success = false;
+                        }
+                    }
+
+                    // Display appropriate message based on the update result
+                    if (success) {
+                        JOptionPane.showMessageDialog(UpdatePersonalInformation.this, "Your personal information updated successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(UpdatePersonalInformation.this, "Error updating your personal information.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+
+        // Set the frame to be visible
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new UpdatePersonalInformation();
+            }
+        });
+    }
+}
