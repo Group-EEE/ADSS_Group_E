d9bb37634da69e322bc2a6243c35aa1324e607da
LironShemen <lironmir@post.bgu.ac.il>
2023-06-12 15:53:48 +0300

    12.6

--- dev/src/InventoryModule/Business/OrderReport.java
+++ dev/src/InventoryModule/Business/OrderReport.java
@@ -13,29 +13,14 @@ import static SuppliersModule.Business.Generator.OrderGenerator.makeOrderFromOrd
 public class OrderReport extends Report{
     private List<Integer> barcodes;
     private List<Integer> amount;
+    private String Reporter;
     //constructor -  creates a report of all products that needs to be ordered in the store
     //the constructor will print the report
     public OrderReport(String reporter){
         super(reporter); //inherit from abstract report class
+        Reporter = reporter;
         barcodes = new ArrayList<>();
         amount = new ArrayList<>();
-        System.out.println("***** Products Order Report *****");
-        System.out.println("Report Date: "+ this.getDate().toLocalDate());
-        System.out.println("Reporter : "+ reporter);
-        //for every product
-        for(int i = 0; i< ProductController.getProducts().size(); i++){
-            //check for all of its specific products if the amount of not defected products
-            //(on shelf and in warehouse) is equal or less than the minimum amount
-            if(ProductController.getProducts().get(i).getSpecificProducts().size() <= ProductController.getProducts().get(i).getMinimum_Amount()){
-                System.out.println("Product's Barcode: " +ProductController.getProducts().get(i).getBarcode() +
-                        " Product's Name: " + ProductController.getProducts().get(i).getPName() +
-                        " Product's Amount: " + ProductController.getProducts().get(i).getSpecificProducts().size() +
-                        " Product's Manufacturer: " + ProductController.getProducts().get(i).getManufacturer());
-                barcodes.add(ProductController.getProducts().get(i).getBarcode());
-                amount.add((ProductController.getProducts().get(i).getMinimum_Amount()+20)-ProductController.getProducts().get(i).getSpecificProducts().size());
-            }
-        }
-        ReportController.addReport(this); //add the report to report controller to save its details
     }
 
     //this function returns the report type, used to print all reports' information
@@ -59,7 +44,20 @@ public class OrderReport extends Report{
         String details = "";
         details = details + "***** Products Order Report *****" + "\n";
         details = details + "Report Date: "+ this.getDate().toLocalDate() + "\n";
-
+        details += "Reporter : "+ Reporter;
+        for(int i = 0; i< ProductController.getProducts().size(); i++){
+            //check for all of its specific products if the amount of not defected products
+            //(on shelf and in warehouse) is equal or less than the minimum amount
+            if(ProductController.getProducts().get(i).getSpecificProducts().size() <= ProductController.getProducts().get(i).getMinimum_Amount()){
+                details += "Product's Barcode: " +ProductController.getProducts().get(i).getBarcode() + "\n";
+                details += "Product's Name: " + ProductController.getProducts().get(i).getPName() + "\n";
+                details += " Product's Amount: " + ProductController.getProducts().get(i).getSpecificProducts().size() + "\n";
+                details += " Product's Manufacturer: " + ProductController.getProducts().get(i).getManufacturer() + "\n";
+                barcodes.add(ProductController.getProducts().get(i).getBarcode());
+                amount.add((ProductController.getProducts().get(i).getMinimum_Amount()+20)-ProductController.getProducts().get(i).getSpecificProducts().size());
+            }
+        }
+        ReportController.addReport(this); //add the report to report controller to save its details
         return details;
     }
 
